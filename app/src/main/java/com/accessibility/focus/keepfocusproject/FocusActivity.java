0b3f660ec95001f87243ddc574d1242c008cfd6c
nguyenthong <nvthong.hn@gmail.com>
2016-08-30 23:13:19 +0700

    thong.nv

--- app/src/main/java/com/accessibility/focus/keepfocusproject/FocusActivity.java
+++ app/src/main/java/com/accessibility/focus/keepfocusproject/FocusActivity.java
@@ -9,5 +9,6 @@ public class FocusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_focus);
+        //This comment create by thong.nv, try make zero to hero =))
     }
 }
