b0896cfb2ccbad0709b7f7f17103a4728f08bdd9
vinhmv <vinhbka92@gmail.com>
2016-09-08 02:51:52 +0700

    Create layout TermsAndConditions & SetupMode

--- app/src/main/java/com/android/keepfocus/utils/MainUtils.java
+++ app/src/main/java/com/android/keepfocus/utils/MainUtils.java
@@ -4,4 +4,5 @@ package com.android.keepfocus.utils;
  * Created by nguyenthong on 9/6/2016.
  */
 public class MainUtils {
+    public static final String TERMS_AND_CONDITIONS = "terms_and_conditions";
 }
